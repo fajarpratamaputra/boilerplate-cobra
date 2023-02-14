1f470e4bef9efa4b3a3f085727dc82bb761f8d15
Jeremy <jeremylombogia7@gmail.com>
2023-02-14 17:18:35 +0700

    fix: sort by score

--- lineup/lineup_calculator_helper.go
+++ lineup/lineup_calculator_helper.go
@@ -1,10 +1,22 @@
 package lineup
 
-func ToLineup(calculateResult *map[int]float64) []int {
-	var lineup []int
-	for i, _ := range *calculateResult {
-		lineup = append(lineup, i)
+import "sort"
+
+func SortByValueDescending(calculateResult *map[int]float64) []int {
+	var keys []int
+	for key := range *calculateResult {
+		keys = append(keys, key)
 	}
+
+	sort.Slice(keys, func(i, j int) bool {
+		return (*calculateResult)[keys[i]] > (*calculateResult)[keys[j]]
+	})
+
+	return keys
+}
+
+func ToLineup(calculateResult *map[int]float64) []int {
+	lineup := SortByValueDescending(calculateResult)
 
 	return lineup
 }
